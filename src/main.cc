71413c396c4d8e97408ff87316676cce1de23181
Inti Glez <inti.gonzalez_herrera@irisa.fr>
2013-12-29 02:28:45 +0100

    Adding option for testing.
    It is esenciatially an option to execute a given method in a class.
    The rest of the work is done in a shell script.
    
    Also, it is not longer possible to use interpretation method.
    The code had not been removed but the argument was.
    Interpretation is considered OBSOLTED :-)

--- src/main.cc
+++ src/main.cc
@@ -27,102 +27,113 @@ extern int optind;
 int main(int argc, char* argv[])
 {
 	if (argc == 1) {
-		cerr << "Usage: "<< argv[0] <<" [-i|-j] class_name [classes_paths]*" << endl;
+		cerr << "Usage: "<< argv[0] <<" [-t] class_name [classes_paths]*" << endl;
 		return -1;
 	}
 	int c = 1;
-	bool useNative = false;
+	bool testing = false;
 	while (true) {
-	   c = getopt(argc, argv, "ij");
+	   c = getopt(argc, argv, "t");
 	   if (c == -1) break;
 	   switch (c) {
-	   case 'i':
-	   case 'j':
-		   useNative = (c == 'j');
+	   case 't':
+		   testing = true;
 		   break;
 	   case '?':
 	   default:
-		   cerr << "Usage: "<< argv[0] <<"  [-i|-j] class_name [classes_paths]*" << endl;
+		   cerr << "Usage: "<< argv[0] <<"  [-t] class_name [classes_paths]*" << endl;
 		   return -1;
 	   }
 	}
-//	ios_base::sync_with_stdio(true);
+
 	ClassLoader::Instance()->AddPath(".");
-	for (int i = optind+1; i < argc ; i++)
+
+	int clazzIndex = (testing)? (optind + 1) : optind;
+
+	for (int i = clazzIndex+1; i < argc ; i++)
 		ClassLoader::Instance()->AddPath(argv[i]);
 
 	Space::instance()->setSpaceSize(4*1024*1024);
-	
-	JvmExecuter* exec = JvmJit::instance(); //new JvmJit(ClassLoader::Instance(), Space::instance());
+	JvmExecuter* exec = JvmJit::instance();
 
-	string main_class(argv[optind]);
-
-	ClassFile* cf = exec->loadAndInit(main_class);// ClassLoader::Instance()->getClass(argv[optind]);
+	string main_class(argv[clazzIndex]);
+	ClassFile* cf = exec->loadAndInit(main_class);
 
 	if (cf == nullptr) {
 		cerr << "Wrong class file: " << argv[optind] << '\n';
 		std::exit(1);
 	}
 
-	int result;
-	JvmExecuter::execute(cf,"testingGetField","()I",(JvmJit*)exec, [&result] (JvmExecuter* exec, void * addr) {
-		int(*a)() = (int(*)())addr;
-		int r = a();
-		result = r;
-	});
-	cout << "testingGetField() = " << result << endl;
+	if (testing) {
+		string method_name = argv[clazzIndex - 1];
+		int result;
+		JvmExecuter::execute(cf,method_name.c_str(),"()I",(JvmJit*)exec, [&result] (JvmExecuter* exec, void * addr) {
+			int(*a)() = (int(*)())addr;
+			int r = a();
+			result = r;
+		});
+		cout << result << endl;
+	}
+	else {
+		int result;
+		JvmExecuter::execute(cf,"testingGetField","()I",(JvmJit*)exec, [&result] (JvmExecuter* exec, void * addr) {
+			int(*a)() = (int(*)())addr;
+			int r = a();
+			result = r;
+		});
+		cout << "testingGetField() = " << result << endl;
 
-	JvmExecuter::execute(cf,"main","([Ljava/lang/String;)V",(JvmJit*)exec, [] (JvmExecuter* exec, void * addr) {
-		void(*a)(Objeto) = (void(*)(Objeto))addr;
-		a(nullptr);
-	});
+		JvmExecuter::execute(cf,"main","([Ljava/lang/String;)V",(JvmJit*)exec, [] (JvmExecuter* exec, void * addr) {
+			void(*a)(Objeto) = (void(*)(Objeto))addr;
+			a(nullptr);
+		});
 
-	JvmExecuter::execute(cf,"main","([Ljava/lang/String;)V",(JvmJit*)exec, [] (JvmExecuter* exec, void * addr) {
-		void(*a)(Objeto) = (void(*)(Objeto))addr;
-		a(nullptr);
-	});
+		JvmExecuter::execute(cf,"main","([Ljava/lang/String;)V",(JvmJit*)exec, [] (JvmExecuter* exec, void * addr) {
+			void(*a)(Objeto) = (void(*)(Objeto))addr;
+			a(nullptr);
+		});
 
-	JvmExecuter::execute(cf,"accessingArray","(I[I)I",(JvmJit*)exec, [&result] (JvmExecuter* exec, void * addr) {
-		Objeto array = newRawArray(T_INT, 20);
-		int value1 = 1;
-		int value2 = 2;
-		ObjectHandler::instance()->assignArrayElement(array, 0 , &value1);
-		ObjectHandler::instance()->assignArrayElement(array, 1 , &value2);
-		int(*a)(int, Objeto) = (int(*)(int,Objeto))addr;
-		int r = a(1,array);
-		result = r;
-	});
-	cout << "accessingArray(1, Arr[20]) : " << result << endl;
+		JvmExecuter::execute(cf,"accessingArray","(I[I)I",(JvmJit*)exec, [&result] (JvmExecuter* exec, void * addr) {
+			Objeto array = newRawArray(T_INT, 20);
+			int value1 = 1;
+			int value2 = 2;
+			ObjectHandler::instance()->assignArrayElement(array, 0 , &value1);
+			ObjectHandler::instance()->assignArrayElement(array, 1 , &value2);
+			int(*a)(int, Objeto) = (int(*)(int,Objeto))addr;
+			int r = a(1,array);
+			result = r;
+		});
+		cout << "accessingArray(1, Arr[20]) : " << result << endl;
 
-	int n = 6;
-	JvmExecuter::execute(cf,"factorial","(I)I",(JvmJit*)exec, [&result,n] (JvmExecuter* exec, void * addr) {
-		int(*a)(int) = (int(*)(int))addr;
-		int r = a(n); // for some crazy reason I cannot assign result = a(n)
-		result = r;
-	});
-	cout << "factorial(" << n << ") = " << result << endl;
+		int n = 6;
+		JvmExecuter::execute(cf,"factorial","(I)I",(JvmJit*)exec, [&result,n] (JvmExecuter* exec, void * addr) {
+			int(*a)(int) = (int(*)(int))addr;
+			int r = a(n); // for some crazy reason I cannot assign result = a(n)
+			result = r;
+		});
+		cout << "factorial(" << n << ") = " << result << endl;
 
-	JvmExecuter::execute(cf,"anotherTest","()I",(JvmJit*)exec, [&result] (JvmExecuter* exec, void * addr) {
-		int(*a)() = (int(*)())addr;
-		int r = a(); // for some crazy reason I cannot assign result = a(n)
-		result = r;
-	});
-	cout << "anotherTest() = " << result << endl;
+		JvmExecuter::execute(cf,"anotherTest","()I",(JvmJit*)exec, [&result] (JvmExecuter* exec, void * addr) {
+			int(*a)() = (int(*)())addr;
+			int r = a(); // for some crazy reason I cannot assign result = a(n)
+			result = r;
+		});
+		cout << "anotherTest() = " << result << endl;
 
-	JvmExecuter::execute(cf,"testingNew","()I",(JvmJit*)exec, [&result] (JvmExecuter* exec, void * addr) {
-		int(*a)() = (int(*)())addr;
-		int r = a(); // for some crazy reason I cannot assign result = a(n)
-		result = r;
-	});
-	cout << "testingNew() = " << result << endl;
+		JvmExecuter::execute(cf,"testingNew","()I",(JvmJit*)exec, [&result] (JvmExecuter* exec, void * addr) {
+			int(*a)() = (int(*)())addr;
+			int r = a(); // for some crazy reason I cannot assign result = a(n)
+			result = r;
+		});
+		cout << "testingNew() = " << result << endl;
 
-	JvmExecuter::execute(cf,"testingNew2","()I",(JvmJit*)exec, [&result] (JvmExecuter* exec, void * addr) {
-		int(*a)() = (int(*)())addr;
-		int r = a(); // for some crazy reason I cannot assign result = a(n)
-		result = r;
-	});
-	cout << "testingNew2() = " << result << endl;
-
+		JvmExecuter::execute(cf,"testingNew2","()I",(JvmJit*)exec, [&result] (JvmExecuter* exec, void * addr) {
+			int(*a)() = (int(*)())addr;
+			int r = a(); // for some crazy reason I cannot assign result = a(n)
+			result = r;
+		});
+		cout << "testingNew2() = " << result << endl;
+	}
 	ClassLoader::Release();
 	return 0;
 }
