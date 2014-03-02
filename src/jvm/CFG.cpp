8f5503561c617be199d382e3dd4a77a7a0843243
Inti Gonzalez-Herrera <inti.glez@gmail.com>
2014-03-02 14:50:23 +0100

    Structure to represent control flow graph

---
+++ src/jvm/CFG.cpp
@@ -0,0 +1,23 @@
+/*
+ * CFG.cpp
+ *
+ *  Created on: Jan 7, 2014
+ *      Author: inti
+ */
+
+#include "CFG.h"
+
+namespace jvm {
+
+CFG::CFG()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+CFG::~CFG()
+{
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace jvm */
---
+++ src/jvm/CFG.h
@@ -0,0 +1,36 @@
+/*
+ * CFG.h
+ *
+ *  Created on: Jan 7, 2014
+ *      Author: inti
+ */
+
+#ifndef CFG_H_
+#define CFG_H_
+
+#include "../utilities/graph.h"
+#include "jit/Quadru.h"
+
+
+namespace jvm {
+
+enum cfg_transition_type {
+	jmp_transition, code_order_transition
+};
+
+class CFG: public graph<jit::BasicBlock*, cfg_transition_type> {
+public:
+	CFG();
+	virtual ~CFG();
+
+	int getBBWithLabel(int l) {
+		for (int i = 0 ; i < nodes.size(); ++i)
+			if (nodes[i]->label == l)
+				return i;
+		return -1;
+	}
+};
+
+} /* namespace jvm */
+
+#endif /* CFG_H_ */
